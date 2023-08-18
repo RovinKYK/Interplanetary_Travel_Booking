a20722deb266294c7b01d9380e391ed6ced84eb4
rovinkyk <kykrovin@gmail.com>
2023-08-18 22:29:51 +0530

    test commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'starfly_database',  
         'USER': 'root',  
-        'PASSWORD': 'slsbcsdckr2',  
+        'PASSWORD': '',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
